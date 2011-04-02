b6d023efcde7f9fe4668a81516e10a27d1251daa
David Stevenson <stevend@davidmac.gateway.2wire.net>
2011-04-02 16:59:12 -0700

    Created Trip#travel_fixes to find all travel fixes within a trip with passing spec.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,11 @@
 class Trip < ActiveRecord::Base
   # has_many :activities
   belongs_to :participant
-  belongs_to :start_travel_fix_id, :class_name => "TravelFix"
-  belongs_to :end_travel_fix_id, :class_name => "TravelFix"
+  belongs_to :start_travel_fix, :class_name => "TravelFix"
+  belongs_to :end_travel_fix, :class_name => "TravelFix"
   has_many :questionnaire_records
+
+  def travel_fixes
+    TravelFix.where(["id BETWEEN ? AND ?", start_travel_fix_id, end_travel_fix_id]).where(:participant_id => participant_id)
+  end
 end
