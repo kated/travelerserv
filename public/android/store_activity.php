936837095df6a107de0612002ad0ce89dd5f2a21
Grant McKenzie <grantdmckenzie@gmail.com>
2011-06-23 22:28:47 -0700

    new store_activity handler for android

---
+++ public/android/store_activity.php
@@ -0,0 +1,27 @@
+<?php 
+
+	/* =================================================================================
+	Author: Grant McKenzie (grantmckenzie.com)
+	Client: GeoTrans Lab UCSB
+	Date: April 2011
+	Project: travelerserv
+	Description: Stores android activities.
+	==================================================================================== */
+	require('android_functions.php');
+	date_default_timezone_set('America/Vancouver');
+	error_reporting(E_ALL | E_STRICT);
+
+	$devid = $_POST['uid'];
+	$alat = $_POST['lat'];
+	$alon = $_POST['lon'];
+	$times = $_POST['t'];
+	
+	// DBAddFix($aEnduro, 'Android');		// send it to the database
+	// spitItOut($aEnduro);	// dump to terminal window
+
+	$myFile = "Android_test_activities.txt";
+	$fh = fopen($myFile, 'a') or die("can't open file");
+	fwrite($fh, "Fix: ".$alat .", ". $alon .", ". $devid .", ".$times."\n");
+	fclose($fh);	
+
+?>
