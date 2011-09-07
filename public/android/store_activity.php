3642a190d783996616846c8a48ef36e7edac91db
Grant McKenzie <grantdmckenzie@gmail.com>
2011-09-06 23:31:02 -0700

    login bug fixes
    Was returning 1 when no user/pass were entered.

--- public/android/store_activity.php
+++ public/android/store_activity.php
@@ -15,16 +15,21 @@
 	$alat = $_GET['lat'];
 	$alon = $_GET['lon'];
 	$times = $_GET['t'];
+	
 	$id = $_GET['id']; */	
 
 	$devid = $_POST['uid'];
 	$alat = $_POST['lat'];
 	$alon = $_POST['lon'];
 	$times = $_POST['t'];
-	$id = $_POST['id'];		// If there is an id, then this is the end of an activity
+	
+	$id = $_POST['id'];		
+	$endact = $_POST['endact'];
+	
 
 	mysql_connect("localhost", "enduro3001", "3nduro3001") or die(ErrorLog(mysql_error()));
 	mysql_select_db("travelerserv_production") or die(ErrorLog(mysql_error()));
+	
 	
 	// Get device id, participant id and their most recent trip id
 	$query1 = "SELECT devices.id, devices.participant_id, max(trips.id) as tripid FROM devices, trips WHERE devices.identification = '".$devid."'  AND devices.participant_id = trips.participant_id";
@@ -36,43 +41,69 @@
 	$recenttrip = $row['tripid'];
 	
 	// If No ID, then it is a new activity
+	
 	if ($id == "") {
 		
 		// Insert a new activity into the activities table.
 		$query = "INSERT into activities (participant_id, latitude, longitude, start) VALUES ";
 		$query .= "(".$participant_id.",".$alat.",".$alon.",'".$times."')";
 		mysql_query($query) or die(ErrorLog(mysql_error()));
+		
 		$actID = mysql_insert_id();
 		// return the new activity id
+		
 		echo $actID . "\n";
 	
+		
 		// return the geocoded results
+		
 		echo foursquare($alat, $alon);
+		
 		echo googleMaps($alat, $alon);
+		
 		echo geoNames($alat, $alon);
 		
+		
 		// Insert the activity into the travel_fixes table.
+		
 		$aEnduro = array("",$devid,"","","","",-9,"",-9,1,floatval($alon),floatval($alat),$times); 
+		
 		$query3 = "INSERT into travel_fixes (participant_id, latitude, longitude, altitude, speed, accuracy, device_id, positioning_method, created_at, updated_at, parent_id, parent_type) VALUES ";
 		$query3 .= "(".$participant_id.",".$aEnduro[11].",".$aEnduro[10].",".$aEnduro[8].",".$aEnduro[6].",".$aEnduro[9].",".$device_id.",'Android','".formatDate($aEnduro[12])."','".formatDate($aEnduro[12])."',".$actID.",'activity')";
 		mysql_query($query3) or die(ErrorLog(mysql_error()));
-		
 		// Update the end time of the most recent trip
+		
 		$query4 = "UPDATE trips SET end = '".formatDate($aEnduro[12])."' WHERE id = ".$recenttrip;
+		
 		mysql_query($query4) or die(ErrorLog(mysql_error()));
 		
-	// If there is an ID, then update that activity with the end time.
+	
+		// If there is an ID, then update that activity with the end time.
+	
 	} else {
-		$query = "UPDATE activities SET end = '".$times."' WHERE id = ".$id;
-		mysql_query($query) or die(ErrorLog(mysql_error()));
+		if ($endact == "true") {
+		
+			$query = "UPDATE activities SET end = '".$times."' WHERE id = ".$id;
+			mysql_query($query) or die(ErrorLog(mysql_error()));
+
+		} else {
+			$aEnduro = array("",$devid,"","","","",-9,"",-9,1,floatval($alon),floatval($alat),$times); 
+		
+			$query8 = "INSERT into travel_fixes (participant_id, latitude, longitude, altitude, speed, accuracy, device_id, positioning_method, created_at, updated_at, parent_id, parent_type) VALUES ";
+			$query8 .= "(".$participant_id.",".$aEnduro[11].",".$aEnduro[10].",".$aEnduro[8].",".$aEnduro[6].",".$aEnduro[9].",".$device_id.",'Android','".formatDate($aEnduro[12])."','".formatDate($aEnduro[12])."',".$id.",'activity')";
+			mysql_query($query8) or die(ErrorLog(mysql_error()));
+		}
 	}
+	
 	
 	mysql_free_result($result);
 	mysql_close();	// clean up your damn mess
 
 
 
-	function geoNames($latitude, $longitude) {
+	
+
+   function geoNames($latitude, $longitude) {
 	   $geocodeURL = "http://api.geonames.org/findNearby?lat=".$latitude."&lng=".$longitude."&radius=0.5&username=grantdmckenzie&maxRows=10";
 	   $ch = curl_init($geocodeURL);
 	   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -95,7 +126,9 @@
 	   }
 	   return $line;
 	}
+	
 	function foursquare($latitude, $longitude) {
+	   
 	   $geocodeURL = "https://api.foursquare.com/v2/venues/search?ll=".$latitude.",".$longitude."&oauth_token=5ZKJSP5O1T01HO0EMVYYX52245QZIGTZELIQRVNYCGYOOR21&v=20110803";
 	   $ch = curl_init($geocodeURL);
 	   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -106,18 +139,27 @@
 	   
 	   if ($httpCode == 200) {
 	   		$obj = json_decode($result);
+	   		
 	   		for($i=0;$i<count($obj->response->venues);$i++) {
+	   			
 	   			$line .= $obj->response->venues[$i]->name . " ";
+	   			
 	   			for($j=0;$j<count($obj->response->venues[$i]->categories);$j++) {
+	   				
 	   				$line .= "(" . $obj->response->venues[$i]->categories[$j]->name . ")";
+	   			
 	   			}
+	   			
 	   			$line .= "\n";
+	   		
 	   		}
 	   } else {
 	     $line = "HTTP_FAIL_$httpCode";
 	   }
 	   return $line;
+	
 	}
+	
 	function googleMaps($latitude, $longitude) {
 	   $geocodeURL = "https://maps.googleapis.com/maps/api/place/search/xml?location=".$latitude.",".$longitude."&radius=500&sensor=false&key=AIzaSyAdstnf_J0wjHZAJZLTItVrO7qQDzgHAYI";
 	   $ch = curl_init($geocodeURL);
